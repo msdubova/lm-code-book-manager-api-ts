e687de6f151b0b824da36189296b0e5f075b2bd2
Your GitHub Usernamemsdubova <femme777@gmail.com>
2023-08-20 23:12:20 +0100

    feat:Implement deleteBook

--- src/app.ts
+++ src/app.ts
@@ -3,5 +3,11 @@ import { router } from "./routes/routes";
 
 export const app = express();
 
+
+app.get("/",(req, res)=> {
+	res.send('Welcome to Minimalist Book Manager')
+} );
+
 app.use(express.json());
 app.use("/api/v1", router);
+
--- src/controllers/books_controller.ts
+++ src/controllers/books_controller.ts
@@ -28,10 +28,39 @@ export const saveBook = async (req: Request, res: Response) => {
 };
 
 // User Story 4 - Update Book By Id Solution
+
 export const updateBook = async (req: Request, res: Response) => {
 	const bookUpdateData = req.body;
 	const bookId = Number.parseInt(req.params.bookId);
 
-	const book = await bookService.updateBook(bookId, bookUpdateData);
-	res.status(204).json(book);
+	try {
+		await bookService.updateBook(bookId, bookUpdateData);
+		const updatedBook = await bookService.getBook(bookId);
+
+		if (!updatedBook) {
+			res.status(404).json({ message: `Book with ID ${bookId} is not found` });
+		} else {
+			res.status(200).json(updatedBook);
+		}
+	} catch (error) {
+		res.status(400).json({ message: (error as Error).message });
+	}
+};
+
+
+
+export const deleteBook = async (req: Request, res: Response) => {
+	const bookId = Number.parseInt(req.params.bookId);
+
+	try {
+		const existingBook = await bookService.getBook(bookId);
+		if (!existingBook) {
+			res.status(404).json({ message: `Book with ID ${bookId} is not found` });
+		} else {
+			await bookService.deleteBook(bookId);
+			res.status(200).json({ message: `Book with ID ${bookId} is removed succesfully` });
+		}
+	} catch (error) {
+		res.status(400).json({ message: (error as Error).message });
+	}
 };
