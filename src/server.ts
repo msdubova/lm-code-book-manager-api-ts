0aa2ee45248511bdd06a665abdd3c5072935ccfd
Your GitHub Usernamemsdubova <femme777@gmail.com>
2023-09-21 18:41:31 +0100

    Fixed env

--- src/server.ts
+++ src/server.ts
@@ -1,29 +1,17 @@
 import * as dotenv from "dotenv";
-
-const environment = process.env.NODE_ENV;
+const environment = process.env.NODE_ENV || "dev";
 dotenv.config({ path: `.env.${environment}` });
 import { app } from "./app";
-// import { populateDummyData } from "./database/database_seed";
+import { populateDummyData } from "./database/database_seed";
+const PORT = process.env.PORT;
 
-const PORT = process.env.PORT || 3000;
 console.log(`🌍 Running in ${environment} environment`);
 
 app.listen(PORT, () => {
+	console.log(`🚂 Express started on port ${PORT}`);
 
 	// Seed the database with some data
-	// if (environment == "dev") {
-	// 	populateDummyData();
-	// }
-		console.log(`🚂 Express started on port ${PORT}`);
+	if (environment === "dev") {
+		populateDummyData();
+	}
 });
-
-
-
-
-
-
-
-
-
-
-
